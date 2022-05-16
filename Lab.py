5603ae81bbf1e1cb8ef64fbaaaeeebf8093606bf
t h <taif@gmail.com>
2022-05-16 15:28:46 +0300

    def hierarchical_pattern(number:int):

---
+++ Lab.py
@@ -0,0 +1,14 @@
+
+
+def hierarchical_pattern(number:int):
+ ''' function that takes 1 parameter of type int , 
+ then it prints out the result hirarchical pattern.
+     '''
+ for i in range(number,-number,-1):
+    for j in range(i,0,-1):
+        # display number
+        print(j, end=' ')
+    # new line after each row
+    print('')
+hierarchical_pattern(5)
+print(hierarchical_pattern.__doc__)
