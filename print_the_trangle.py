503e887b9ca9676834c7929172fe8dee6774f6f4
Jawaher Alqotym <63616896+jawaher-alqotym@users.noreply.github.com>
2022-05-16 15:01:09 +0300

    Update print_the_trangle.py

--- print_the_trangle.py
+++ print_the_trangle.py
@@ -1,4 +1,7 @@
 def print_trangle(num: int = 5) -> str:
+    '''
+    this function  takes 1 parameter of type int , then it prints out as a trangle.
+    '''
     str_num = ''
     for i in range(num, 0, -1):
         for j in range(i, 0, -1):
