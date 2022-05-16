ab78dd5c8aaca94ca542d09b35e4c8ee66461725
Faisal othman <Faisalalsneed@gmail.com>
2022-05-16 16:22:12 +0300

    add Document

--- Function.py
+++ Function.py
@@ -1,11 +1,13 @@
 
 
-def c(rows=int):
-    
+def Pyramid(rows=int):
+    '''
+    this function used to order number
+    '''
+
     for i in range(0,rows + 1):
         for j in range(rows - i,0,-1):
             print( j,end=' ')
         print()
-
-
-c(5)
+Pyramid(5)
+print(Pyramid.__doc__)
