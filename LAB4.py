4aaeba49c46891d67f6be557befbb824084e391c
Abdulaziz Altariqi <AbdulazizAlturaiqi@gmail.com>
2022-05-16 15:59:28 +0300

    edit in line 8

--- LAB4.py
+++ LAB4.py
@@ -5,7 +5,7 @@ def pattern_num(num:int):
     '''
     for i in range (num, 0, -1):
         for j in range(i, 0, -1):
-            print(j, end=" ", )
+            print(j, end=" ")
         print ("")
         
 
