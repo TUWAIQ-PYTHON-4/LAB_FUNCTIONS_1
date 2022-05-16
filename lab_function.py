06527ee977bfc46623fc6b518f44d60ea7c223d9
Emtinan98 <102264151+Emtinan98@users.noreply.github.com>
2022-05-16 15:28:47 +0300

    ADDED FUNCTION FILE

---
+++ lab_function.py
@@ -0,0 +1,8 @@
+def numbers(num:int)->int:
+    '''This function ...''' 
+    for i in range(num + 1, 1, -1):    
+     for j in range(1, i - 1):  
+        print(j, end=' ')  
+     print(" ")  
+
+numbers(6)    
