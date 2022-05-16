fa81fc2937dc6cf3081bf1e7fd8605bbf0315a3b
Abdulaziz Altariqi <AbdulazizAlturaiqi@gmail.com>
2022-05-16 15:56:52 +0300

    LAB4 done

---
+++ LAB4.py
@@ -0,0 +1,13 @@
+
+def pattern_num(num:int):
+    '''
+    this function used to printing the number in hierarchical pattern
+    '''
+    for i in range (num, 0, -1):
+        for j in range(i, 0, -1):
+            print(j, end=" ", )
+        print ("")
+        
+
+pattern_num(5)
+print(pattern_num.__doc__)
