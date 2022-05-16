76ab63a2f7177460bfeb1b87263b89f2c09f2eaf
Emtinan98 <102264151+Emtinan98@users.noreply.github.com>
2022-05-16 16:17:56 +0300

    Update func lab

--- lab_function.py
+++ lab_function.py
@@ -1,8 +1,10 @@
 def numbers(num:int)->int:
-    '''This function return the reseve pattren''' 
-    for i in range(num + 1, 1, -1):    
-     for j in range(1, i - 1):  
-        print(j, end=' ')  
-     print(" ")  
-
-numbers(6)    
+    '''This function return reseve pattern ''' 
+    for i in range(0,num):    
+     for j in range(num,0,-1):   
+         print(j, end=' ') 
+     print(" ") 
+     num-=1
+print(numbers.__doc__)    
+       
+numbers(5)   
