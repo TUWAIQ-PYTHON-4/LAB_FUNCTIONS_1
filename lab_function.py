54799f01f7ff849ef5fd90e97ff4815c49710951
Emtinan98 <102264151+Emtinan98@users.noreply.github.com>
2022-05-16 15:39:36 +0300

    update

--- lab_function.py
+++ lab_function.py
@@ -1,5 +1,5 @@
 def numbers(num:int)->int:
-    '''This function ...''' 
+    '''This function return the reseve pattren''' 
     for i in range(num + 1, 1, -1):    
      for j in range(1, i - 1):  
         print(j, end=' ')  
