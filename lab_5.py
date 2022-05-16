0313b29cfae93fc960ad6a6b69f8ed96d71d8771
rahaf-adel <rahafadeel5@gmail.com>
2022-05-16 15:52:32 +0300

    This is solve Lab 5

---
+++ lab_5.py
@@ -0,0 +1,19 @@
+# Create a function that takes 1 parameter of type int , then it prints out the result formatted like the following patter (if we give it 5 for example): from traceback import print_last
+
+
+def print_full_numbers(number:int=5):
+    """
+        This is function takes 1 parameter of type int , then it prints out the result formatted like the following patter 
+    """
+
+    for i in range(0,number):
+        for j in range(number, i,-1):
+            print(j, end=" ")
+        
+        print("")
+
+print_full_numbers(6)
+
+#Document the newly created function. describe what it does, then print the documentation.
+print(print_full_numbers.__doc__)
+
